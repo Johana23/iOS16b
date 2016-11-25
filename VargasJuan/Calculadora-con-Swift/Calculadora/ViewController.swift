3dcbeadb503f858f7f238a20aabcd8ece2ac434d
Juan Samuel Vargas <juan.samuel.vargas@gmail.com>
2016-11-25 15:37:10 -0800

    Calculadora

---
+++ VargasJuan/Calculadora-con-Swift/Calculadora/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Calculadora
+//
+//  Created by isc on 11/23/16.
+//  Copyright © 2016 ITT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber: Bool = false
+    
+    @IBAction func appendDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        }
+        else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+
+}
