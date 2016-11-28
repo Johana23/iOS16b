a66a74e2d91ad6a148e5d3d773a6f741067f6bef
ayrebi <cuanaloayrebi@gmail.com>
2016-11-28 14:32:50 +0100

    App Calculadora y 10 practicas

---
+++ CuanaloGonzalez/Practicas/Practica41/Practica41/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  Practica41
+//
+//  Created by CuanaloGonzaleAyrebi on 11/27/16.
+//  Copyright © 2016 PDD. All rights reserved.
+//
+
+import Foundation
+
+let kilogramos = 212.0
+
+let resultado = "Numero de libras en 212 kilogramos = \((kilogramos/2.2))"
+
+print (resultado)
+
+
