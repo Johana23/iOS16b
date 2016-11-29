602b8bd8336dda2dad16e9f607b31d36302390c2
Francisco Valdez <isc.valdezfrancisco@gmail.com>
2016-11-28 17:18:16 -0800

    add 6_13

---
+++ FranciscoValdez/ejercicios/6_13.swift
@@ -0,0 +1,11 @@
+// capitulo 6 ejercicio 13
+// Imprimir los primeros 25 numeros de la secuencia 3, 6, 9, 12, 15
+import Foundation
+
+var i=1
+var sum=0
+while i <= 25 {
+    sum = 3 * i
+    print (sum)
+    i = i+1
+}
