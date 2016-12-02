03c470e61fabe0a81f67ef978bb659de015fd296
rafa <cristhian.cerda.lopez@gmail.com>
2016-12-01 16:13:09 -0800

    CALCULADORA

---
+++ FernandoDominguez/calculadora/calculadora/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  calculadora
+//
+//  Created by christopher juarez fraijo on 11/22/16.
+//  Copyright © 2016 itt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lablelResult: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    
+    @IBAction func num(_ sender: UIButton) {
+        
+        
+    }
+    
+    
+    @IBAction func op(_ sender: UIButton) {
+    }
+    
+
+    @IBAction func clear(_ sender: UIButton) {
+    }
+
+    
+    
+    
+}
+
