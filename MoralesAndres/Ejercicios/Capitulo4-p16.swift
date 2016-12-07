4b9a236814709709cfbfc81da7b049d9b43b31fd
AndresMOITT <morales094andres@gmail.com>
2016-12-07 13:48:12 -0800

    dd
    s

--- MoralesAndres/Ejercicios/Capitulo4-p16.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-func area(a:Double, b:Double) -> Double{
-    return (a * b)/2
-}
-var total = area(a:5.1, b:10.2)
-
-var red = "Area del triangulo con base = 5.1 y altura = 10.2 "
--- MoralesAndres/Ejercicios/Capitulo4-p21.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-var radianes:Double=0.5
-print("radianes: ",radianes)
-var pi:Double = 3.1416
-var operacion:Double = 0
-operacion = radianes*(180/pi)
-print("grados: ",operacion)
