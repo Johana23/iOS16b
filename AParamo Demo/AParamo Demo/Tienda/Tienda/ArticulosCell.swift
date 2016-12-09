164f11becacc581a88903c18cb71dc56ced0ccb9
aimee1293 <isc.aimee.paramo@gmail.com>
2016-12-09 00:05:21 -0800

    Add files via upload

---
+++ AParamo Demo/AParamo Demo/Tienda/Tienda/ArticulosCell.swift
@@ -0,0 +1,16 @@
+//
+//  CellArticulos.swift
+//  Tienda
+//
+//
+
+import UIKit
+
+class ArticulosCell: UITableViewCell {
+
+    
+    @IBOutlet weak var labelTitulo: UILabel!
+    @IBOutlet weak var labelValor: UILabel!
+    @IBOutlet weak var labelExistencias: UILabel!
+    
+}
---
+++ AParamo Demo/AParamo Demo/Tienda/Tienda/Utilities.swift
@@ -0,0 +1,57 @@
+//
+//  Utilities.swift
+//  Tienda
+//
+//
+
+import UIKit
+
+class Utilities: NSObject {
+
+    class func getPath(fileName: String) -> String {
+        
+        let documentsURL = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)[0]
+        let fileURL = documentsURL.URLByAppendingPathComponent(fileName)
+        
+        return fileURL.path!
+    }
+    
+    class func copyFile(fileName: NSString) {
+        let dbPath: String = getPath(fileName as String)
+        let fileManager = NSFileManager.defaultManager()
+        if !fileManager.fileExistsAtPath(dbPath) {
+            
+            let documentsURL = NSBundle.mainBundle().resourceURL
+            let fromPath = documentsURL!.URLByAppendingPathComponent(fileName as String)
+            
+            var error : NSError?
+            
+            do {
+                try fileManager.copyItemAtPath(fromPath.path!, toPath: dbPath)
+            } catch let error1 as NSError {
+                error = error1
+            }
+            
+            let alert: UIAlertView = UIAlertView()
+            if (error != nil) {
+                alert.title = "Error al copiar la Base de datos"
+                alert.message = error?.localizedDescription
+            } else {
+                alert.title = "Copia Exitosa"
+                alert.message = "Base de datos copiada con éxito"
+            }
+            alert.delegate = nil
+            alert.addButtonWithTitle("Ok")
+            alert.show()
+        }
+    }
+    
+    class func invokeAlertMethod(strTitle: NSString, strBody: NSString, delegate: AnyObject?) {
+        let alert: UIAlertView = UIAlertView()
+        alert.message = strBody as String
+        alert.title = strTitle as String
+        alert.delegate = delegate
+        alert.addButtonWithTitle("Ok")
+        alert.show()
+    }
+}
